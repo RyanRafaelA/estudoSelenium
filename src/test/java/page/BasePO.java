e10084b4bab2b3d8b13a4933995577bdcfa97a1b
RyanRafaelA <ryrafael461@gmail.com>
2026-01-28 16:23:33 -0300

    Inicializando o LoginPO

--- src/test/java/page/BasePO.java
+++ src/test/java/page/BasePO.java
@@ -6,7 +6,7 @@ import org.openqa.selenium.support.PageFactory;
 public abstract class BasePO {
     protected WebDriver driver;
 
-    protected BasePO(WebDriver driver){
+    public BasePO(WebDriver driver){
         this.driver = driver;
         PageFactory.initElements(driver, this);
     }
---
+++ src/test/java/page/LoginPO.java
@@ -0,0 +1,24 @@
+package page;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class LoginPO extends BasePO{
+    @FindBy(id = "email")
+    public WebElement inputEmail;
+
+    @FindBy(id = "senha")
+    public WebElement inputSenha;
+
+    @FindBy(id = "btn-entrar")
+    public WebElement buttonEntrar;
+
+    @FindBy(css = "form.form-login>div.alert>span")
+    public WebElement spanMensagem;
+
+
+    public LoginPO(WebDriver driver) {
+        super(driver);
+    }
+}
